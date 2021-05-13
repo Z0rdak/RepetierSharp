f7005a232266b4e2f3295d03206c0681cf3bfd6c
Marcel Bartholet <Marcel.Bartholet@mni.thm.de>
2021-05-13 22:54:37 +0200

    add classes with constants for literals

---
+++ RepetierMqtt/Models/Commands/CommandConstants.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepetierMqtt.Models.Commands
+{
+    public static class CommandConstants
+    {
+        public const string LOGIN = "login";
+        public const string LOGOUT = "logout";
+        public const string LIST_PRINTER = "listPrinter";
+        public const string STATE_LIST = "stateList";
+        public const string RESPONSE = "response";
+        public const string MESSAGES = "messages";
+        public const string LIST_MODELS = "listModels";
+        public const string LIST_JOBS = "listJobs";
+
+    }
+}
---
+++ RepetierMqtt/Models/Events/EventConstants.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepetierMqtt.Models.Events
+{
+    public static class EventConstants
+    {
+        public const string LOGOUT = "logout";
+        public const string LOGIN_REQUIRED = "loginRequired";
+        public const string USER_CREDENTIALS = "userCredentials";
+        public const string PRINTER_LIST_CHANGED = "printerListChanged";
+        public const string MESSAGES_CHANGED = "messagesChanged";
+        public const string LOG = "log";
+        public const string JOBS_CHANGED = "jobsChanged";
+        public const string JOB_FINISHED = "jobFinished";
+        public const string JOB_KILLED = "jobKilled";
+        public const string JOB_STARTED = "jobStarted";
+        public const string STATE = "state";
+        public const string TEMP = "temp";
+        public const string CHANGE_FILAMENT = "changeFilament";
+    }
+}
