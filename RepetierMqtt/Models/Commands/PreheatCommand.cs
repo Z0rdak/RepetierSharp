c89be7879dc7e36cd5969fd326b1fa0a2d6be089
Marcel Bartholet <Marcel.Bartholet@mni.thm.de>
2021-10-13 20:45:23 +0200

    add more print control command implementations

---
+++ RepetierMqtt/Models/Commands/PreheatCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepetierMqtt.Models.Commands
+{
+    public class PreheatCommand : ICommandData
+    {
+
+        public int ExtruderNo { get; set; }
+        public int HeatedBedNo { get; set; }
+        public int HeatedChamberNo { get; set; }
+
+        public string CommandIdentifier => CommandConstants.PREHEAT;
+
+        public PreheatCommand(int extruderNo, int heatedBedNo, int heatedChamberNo)
+        {
+            ExtruderNo = extruderNo;
+            HeatedBedNo = heatedBedNo;
+            HeatedChamberNo = heatedChamberNo;
+        }
+    }
+
+    public enum ExtruderConstants
+    {
+        No = 0,
+        All = 1,
+        Active = 2,
+    }
+}
