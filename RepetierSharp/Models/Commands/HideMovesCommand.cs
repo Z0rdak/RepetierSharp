51b7f713bdf09254822f230f27c994efc6870030
Marcel Bartholet <Marcel.Bartholet@mni.thm.de>
2022-02-01 13:36:25 +0100

    add move commands

---
+++ RepetierSharp/Models/Commands/HideMovesCommand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace RepetierSharp.Models.Commands
+{
+    public class HideMovesCommand : ICommandData
+    {
+        [JsonIgnore]
+        public string CommandIdentifier => CommandConstants.HIDE_MOVES;
+
+        public HideMovesCommand() { }
+
+        public static HideMovesCommand Instance => new HideMovesCommand();
+    }
+}
---
+++ RepetierSharp/Models/Commands/SendMovesCommand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace RepetierSharp.Models.Commands
+{
+    public class SendMovesCommand : ICommandData
+    {
+        [JsonIgnore]
+        public string CommandIdentifier => CommandConstants.SEND_MOVES;
+
+        public SendMovesCommand() { }
+
+        public static SendMovesCommand Instance => new SendMovesCommand();
+    }
+}
