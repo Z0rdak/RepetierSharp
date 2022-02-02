dfbc04fa956f6b63306bd457d2f9bef68d45c5f8
Marcel Bartholet <Marcel.Bartholet@mni.thm.de>
2022-02-02 21:22:34 +0100

    cleanup repetiersharp example

--- Example/RepetierSharp.cs
+++ Example/RepetierSharp.cs
@@ -17,41 +17,29 @@ namespace RepetierSharp.Example
         public static void Main(string[] args)
         {
             RepetierConnection repetierConnection = new RepetierConnectionBuilder()
-                //.WithHost("sfm-evotech-el11.mni.thm.de", 3344)
                 .WithHost("demo.repetier-server.com",4006)
                 .WithApiKey("7075e377-7472-447a-b77e-86d481995e7b")
-                //.WithApiKey("fe2c8a4c-9ca1-4511-9779-6b98a635eacc")
                 .QueryPrinterInterval(RepetierTimer.Timer60)             
                 .QueryStateInterval(RepetierTimer.Timer30)
-                //.WithCredentials("Repetier-Admin", "sfm_2020", true)
-                //.UseLang("US")
-                //.WithTls(true)
                 .Build();
 
-            repetierConnection.Connect();
-
-            //repetierConnection.UploadAndStartPrint(@"D:\Fuss_0.2mm_ABS_EL-11_2h24m.gcode", "Cartesian");
-            //repetierConnection.UploadGCode(@"D:\Fuss_0.2mm_ABS_EL-11_2h24m.gcode", "Default", "Delta");
-
+            
             repetierConnection.OnEvent += (string eventName, string printer, IRepetierEvent eventData) =>
             {
-                if (!(eventName == "temp"))
-                {
-                    //Console.WriteLine($"Event={eventName}, Printer={printer}");
-                }
+
             };
 
             repetierConnection.OnResponse += (id, command, response) =>
             {
-                //Console.WriteLine($"Receive [{id}]: {command}");
+                
             };
-
 
             repetierConnection.OnRawEvent += (e, p, data) =>
             {                
-               
+                if (!(e == "temp"))
+                {
                     Console.WriteLine($"Event [{e}@{((string.IsNullOrEmpty(p)) ? "" : p)}]: {Encoding.UTF8.GetString(data)}");
-                
+                }                 
             };
 
             repetierConnection.OnRawResponse += (id, cmd, data) =>
@@ -59,28 +47,17 @@ namespace RepetierSharp.Example
                 Console.WriteLine($"Receive [{id}]: {cmd} | {Encoding.UTF8.GetString(data)}");
             };
 
+            repetierConnection.OnRepetierConnected += () =>
+            {
+                Console.WriteLine("Connected!");
+                repetierConnection.ActivatePrinter("Delta");
+                repetierConnection.EnqueueJob(6);
+            };
+       
+            repetierConnection.Connect();
 
-            // Activate
-            // Deactivate
-            // Stop
-            // Pause (Send)
-            // Continue
-            // SetFan,Extruder,Etc
-            // repetierConnection.EnqueueJob(6);
-
-            // repetierConnection.PreheatAll(); // permission denied with demo server
-            // repetierConnection.Cooldown();// permission denied with demo server
-
-
-
-
-            Thread.Sleep(2000);
-            repetierConnection.SendCommand(new ActivateCommand("Cartesian"));
-            Thread.Sleep(2000);
-
-            repetierConnection.RemoveJob(6);
-
-
+            //repetierConnection.UploadAndStartPrint(@"D:\Fuss_0.2mm_ABS_EL-11_2h24m.gcode", "Delta");
+            //repetierConnection.UploadGCode(@"D:\Fuss_0.2mm_ABS_EL-11_2h24m.gcode", "Default", "Delta");
 
             while (true)
             {
