83b74b862547a0dc5bd5bd4806ce78dcd0a389c4
Marcel Bartholet <Marcel.Bartholet@mni.thm.de>
2022-02-02 20:23:09 +0100

    remove unused method

--- RepetierSharp/Util/ApiConstants.cs
+++ RepetierSharp/Util/ApiConstants.cs
@@ -11,21 +11,5 @@ namespace RepetierSharp.Util
         public const string PRINTER_INFO_PATH = "/printer/info";
 
         public static readonly IRestRequest PRINTER_INFO_REQUEST = new RestRequest(PRINTER_INFO_PATH, Method.GET);
-
-        // TODO: Test
-        // TODO: Is there a request only for upload?
-        public static IRestRequest StartPrintRequest(string gcodeFilePath, string printerName, string apiKey)
-        {
-            // TODO: consider session key in url for printing wihtout api-key?
-            var GCODEFileName = Path.GetFileNameWithoutExtension(gcodeFilePath);
-            return new RestRequest($"/printer/{printerName}", Method.POST)
-                .AddFile("gcode", GCODEFileName)
-                .AddHeader("x-api-key", apiKey)
-                .AddHeader("Content-Type", "multipart/form-data")
-                .AddParameter("a", "upload")
-                .AddParameter("name", GCODEFileName);
-        }
-
-
     }
 }
