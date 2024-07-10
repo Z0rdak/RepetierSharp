12afc43fb205efbaca9e9fe5335664d82d6e09dc
Marcel Bartholet <marcel.bartholet@mni.thm.de>
2024-07-10 10:48:34 +0200

    add permission denied to client events

--- Internal/RepetierClientEvents.cs
+++ Internal/RepetierClientEvents.cs
@@ -1,7 +1,4 @@
 using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using Microsoft.Extensions.Logging;
 using RepetierSharp.Models.Events;
 using RepetierSharp.Models.Messages;
 
@@ -9,97 +6,111 @@ namespace RepetierSharp.Internal
 {
     public sealed class RepetierClientEvents
     {
-        public AsyncEvent<RepetierConnectedEventArgs> ConnectedEvent { get; } = new AsyncEvent<RepetierConnectedEventArgs>();
-        public AsyncEvent<LoginRequiredEventArgs> LoginRequiredEvent = new AsyncEvent<LoginRequiredEventArgs>();
-        public AsyncEvent<LoginResultEventArgs> LoginResultEvent = new AsyncEvent<LoginResultEventArgs>();
-        public AsyncEvent<RepetierEventReceivedEventArgs> RepetierEventReceivedEvent = new AsyncEvent<RepetierEventReceivedEventArgs>();
-        public AsyncEvent<RepetierResponseReceivedEventArgs> RepetierResponseReceivedEvent = new AsyncEvent<RepetierResponseReceivedEventArgs>();
-        public AsyncEvent<RawRepetierEventReceivedEventArgs> RawRepetierEventReceivedEvent = new AsyncEvent<RawRepetierEventReceivedEventArgs>();
-        public AsyncEvent<RawRepetierResponseReceivedEventArgs> RawRepetierResponseReceivedEvent = new AsyncEvent<RawRepetierResponseReceivedEventArgs>();
+        public AsyncEvent<RepetierConnectedEventArgs> ConnectedEvent { get; } = new();
+        public AsyncEvent<LoginRequiredEventArgs> LoginRequiredEvent = new();
+        public AsyncEvent<LoginResultEventArgs> LoginResultEvent = new();
+        public AsyncEvent<PermissionDeniedEventArgs> PermissionDeniedEvent = new();
 
-        /// <summary>
-        /// Event for received events from the repetier server.
-        /// </summary>
-        public sealed class RepetierEventReceivedEventArgs : EventArgs
-        {
-            public RepetierEventReceivedEventArgs(string eventName, string printer, IRepetierEvent? repetierEvent)
-            {
-                EventName = eventName;
-                Printer = printer;
-                RepetierEvent = repetierEvent;
-            }
-
-            public IRepetierEvent? RepetierEvent { get; set; }
-            public string EventName { get; set; }
-            public string Printer { get; set; }
-        }
-        
-        /// <summary>
-        /// Fired whenever an event from the repetier server is received. 
-        /// The payload is the raw event itself (content of the data field of the json from the documentation).
-        /// </summary>
-        public sealed class RawRepetierEventReceivedEventArgs : EventArgs
-        {
-            /// <summary>
-            /// 
-            /// </summary>
-            /// <param name="eventName"> Name of the received event </param>
-            /// <param name="printer"> Printer associated with the event or empty if global </param>
-            /// <param name="eventPayload"> Event payload as byte array </param>
-            public RawRepetierEventReceivedEventArgs(string eventName, string printer, byte[] eventPayload)
-            {
-                EventName = eventName;
-                Printer = printer;
-                EventPayload = eventPayload;
-            }
-
-            public byte[] EventPayload { get; set; }
-            public string EventName { get; set; }
-            public string Printer { get; set; }
-        }
-        
-        public sealed class RepetierResponseReceivedEventArgs : EventArgs
-        {
-            public RepetierResponseReceivedEventArgs(int callbackId, string command, IRepetierMessage? message)
-            {
-                CallbackId = callbackId;
-                Command = command;
-                Message = message;
-            }
-
-            public string Command { get; set; }
-            public int CallbackId { get; set; }
-            public IRepetierMessage? Message { get; set; }
-        }
-        
-        
-        /// <summary>
-        /// Fired whenever a command response from the repetier server is received. 
-        /// The payload is the raw response itself (content of the data field of the json from the documentation).
-        /// </summary>
-        public sealed class RawRepetierResponseReceivedEventArgs : EventArgs
-        {
-            /// <summary>
-            /// 
-            /// </summary>
-            /// <param name="callbackId"> CallBackId to identify the received command response </param>
-            /// <param name="command"> Name of the command associated with the received response </param>
-            /// <param name="responsePayload"> Command response payload </param>
-            public RawRepetierResponseReceivedEventArgs(int callbackId, string command, byte[] responsePayload)
-            {
-                CallbackId = callbackId;
-                Command = command;
-                ResponsePayload = responsePayload;
-            }
-
-            public string Command { get; set; }
-            public int CallbackId { get; set; }
-            public byte[] ResponsePayload { get; set; }
-        }
+        public AsyncEvent<RepetierEventReceivedEventArgs> RepetierEventReceivedEvent = new();
+        public AsyncEvent<RepetierResponseReceivedEventArgs> RepetierResponseReceivedEvent = new();
+        public AsyncEvent<RawRepetierEventReceivedEventArgs> RawRepetierEventReceivedEvent = new();
+        public AsyncEvent<RawRepetierResponseReceivedEventArgs> RawRepetierResponseReceivedEvent = new();
     }
 
-    
-    
+    /// <summary>
+    /// Event which is fired when a command is not permitted for the current sessionId.
+    /// </summary>
+    public sealed class PermissionDeniedEventArgs : EventArgs
+    {
+        public PermissionDeniedEventArgs(int commandId)
+        {
+            CommandId = commandId;
+        }
+
+        public int CommandId { get; set; }
+    }
+
+    /// <summary>
+    /// Event for received events from the repetier server.
+    /// </summary>
+    public sealed class RepetierEventReceivedEventArgs : EventArgs
+    {
+        public RepetierEventReceivedEventArgs(string eventName, string printer, IRepetierEvent? repetierEvent)
+        {
+            EventName = eventName;
+            Printer = printer;
+            RepetierEvent = repetierEvent;
+        }
+
+        public IRepetierEvent? RepetierEvent { get; set; }
+        public string EventName { get; set; }
+        public string Printer { get; set; }
+    }
+
+    /// <summary>
+    /// Fired whenever an event from the repetier server is received. 
+    /// The payload is the raw event itself (content of the data field of the json from the documentation).
+    /// </summary>
+    public sealed class RawRepetierEventReceivedEventArgs : EventArgs
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="eventName"> Name of the received event </param>
+        /// <param name="printer"> Printer associated with the event or empty if global </param>
+        /// <param name="eventPayload"> Event payload as byte array </param>
+        public RawRepetierEventReceivedEventArgs(string eventName, string printer, byte[] eventPayload)
+        {
+            EventName = eventName;
+            Printer = printer;
+            EventPayload = eventPayload;
+        }
+
+        public byte[] EventPayload { get; set; }
+        public string EventName { get; set; }
+        public string Printer { get; set; }
+    }
+
+    public sealed class RepetierResponseReceivedEventArgs : EventArgs
+    {
+        public RepetierResponseReceivedEventArgs(int callbackId, string command, IRepetierMessage? message)
+        {
+            CallbackId = callbackId;
+            Command = command;
+            Message = message;
+        }
+
+        public string Command { get; set; }
+        public int CallbackId { get; set; }
+        public IRepetierMessage? Message { get; set; }
+    }
+
+
+    /// <summary>
+    /// Fired whenever a command response from the repetier server is received. 
+    /// The payload is the raw response itself (content of the data field of the json from the documentation).
+    /// </summary>
+    public sealed class RawRepetierResponseReceivedEventArgs : EventArgs
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="callbackId"> CallBackId to identify the received command response </param>
+        /// <param name="command"> Name of the command associated with the received response </param>
+        /// <param name="responsePayload"> Command response payload </param>
+        public RawRepetierResponseReceivedEventArgs(int callbackId, string command, byte[] responsePayload)
+        {
+            CallbackId = callbackId;
+            Command = command;
+            ResponsePayload = responsePayload;
+        }
+
+        public string Command { get; set; }
+        public int CallbackId { get; set; }
+        public byte[] ResponsePayload { get; set; }
+    }
+
+
     public sealed class LoginResultEventArgs : EventArgs
     {
         public LoginResultEventArgs(LoginResponse loginResult)
@@ -109,7 +120,7 @@ namespace RepetierSharp.Internal
 
         public LoginResponse LoginResult { get; set; }
     }
-    
+
     public sealed class LoginRequiredEventArgs : EventArgs
     {
         public LoginRequiredEventArgs()
@@ -117,7 +128,7 @@ namespace RepetierSharp.Internal
         }
     }
 
-    
+
     public sealed class RepetierConnectedEventArgs : EventArgs
     {
         public RepetierConnectedEventArgs(string sessionId)
