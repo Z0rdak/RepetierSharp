431123e3e0090060a8f7df13a3fbb9ff5d000483
Marcel Bartholet <Marcel.Bartholet@mni.thm.de>
2025-03-19 08:06:33 +0100

    add change filament request event args; add base class for printer event args to reduce duplicate code

--- RepetierSharp/Internal/RepetierPrinterEvents.cs
+++ RepetierSharp/Internal/RepetierPrinterEvents.cs
@@ -16,118 +16,94 @@ namespace RepetierSharp.Internal
         public AsyncEvent<TemperatureChangedEventArgs> TemperatureChangedEvent { get; } = new();
         public AsyncEvent<MovedEventArgs> MovedEvent { get; } = new();
         public AsyncEvent<LayerChangedEventArgs> LayerChangedEvent { get; } = new();
+        public AsyncEvent<ChangeFilamentRequestedEventArgs> ChangeFilamentRequestedEvent { get; } = new();
     }
-    
-    public sealed class LayerChangedEventArgs : EventArgs
+
+    public abstract class PrinterEventArgs : EventArgs
     {
-        public LayerChangedEventArgs(string printer, LayerChanged layerChange)
+        public PrinterEventArgs(string printer)
         {
             Printer = printer;
-            LayerChanged = layerChange;
         }
         public string Printer { get; }
+    }
+    
+    public sealed class ChangeFilamentRequestedEventArgs : PrinterEventArgs
+    {
+        public ChangeFilamentRequestedEventArgs(string printer) : base(printer) { }
+    }
+    
+    public sealed class LayerChangedEventArgs : PrinterEventArgs
+    {
+        public LayerChangedEventArgs(string printer, LayerChanged layerChange) : base(printer)
+        {
+            LayerChanged = layerChange;
+        }
         public LayerChanged LayerChanged { get; }
     }
 
-    public sealed class MovedEventArgs : EventArgs
+    public sealed class MovedEventArgs : PrinterEventArgs
     {
-        public MovedEventArgs(string printer, MoveEntry moveEntry)
+        public MovedEventArgs(string printer, MoveEntry moveEntry) : base(printer)
         {
-            Printer = printer;
             MoveEntry = moveEntry;
         }
-
-        public string Printer { get; }
         public MoveEntry MoveEntry { get; }
     }
 
-    public sealed class TemperatureChangedEventArgs : EventArgs
+    public sealed class TemperatureChangedEventArgs : PrinterEventArgs
     {
-        public TemperatureChangedEventArgs(string printer, TempEntry tempEntry)
+        public TemperatureChangedEventArgs(string printer, TempEntry tempEntry): base(printer) 
         {
-            Printer = printer;
             TempEntry = tempEntry;
         }
-
-        public string Printer { get; }
         public TempEntry TempEntry { get; }
     }
 
-    public sealed class EmergencyStopTriggeredEventArgs : EventArgs
+    public sealed class EmergencyStopTriggeredEventArgs : PrinterEventArgs
     {
-        public EmergencyStopTriggeredEventArgs(string printer)
-        {
-            Printer = printer;
-        }
-
-        public string Printer { get; }
+        public EmergencyStopTriggeredEventArgs(string printer): base(printer) { }
     }
 
-    public sealed class ActivatedEventArgs : EventArgs
+    public sealed class ActivatedEventArgs : PrinterEventArgs
     {
-        public ActivatedEventArgs(string printer)
-        {
-            Printer = printer;
-        }
-
-        public string Printer { get; }
+        public ActivatedEventArgs(string printer): base(printer) { }
     }
 
-    public sealed class DeactivatedEventArgs : EventArgs
+    public sealed class DeactivatedEventArgs : PrinterEventArgs
     {
-        public DeactivatedEventArgs(string printer)
-        {
-            Printer = printer;
-        }
-
-        public string Printer { get; }
+        public DeactivatedEventArgs(string printer): base(printer) { }
     }
 
-
-    public sealed class JobsChangedEventArgs : EventArgs
+    public sealed class JobsChangedEventArgs : PrinterEventArgs
     {
-        public JobsChangedEventArgs(string printer)
-        {
-            Printer = printer;
-        }
-
-        public string Printer { get; }
+        public JobsChangedEventArgs(string printer): base(printer) { }
     }
 
-
-    public sealed class StateChangedEventArgs : EventArgs
+    public sealed class StateChangedEventArgs : PrinterEventArgs
     {
-        public StateChangedEventArgs(string printer, PrinterState printerState)
+        public StateChangedEventArgs(string printer, PrinterState printerState): base(printer)
         {
-            Printer = printer;
             PrinterState = printerState;
         }
-
-        public string Printer { get; }
         public PrinterState PrinterState { get; }
     }
 
-    public sealed class ConditionChangedEventArgs : EventArgs
+    public sealed class ConditionChangedEventArgs : PrinterEventArgs
     {
-        public ConditionChangedEventArgs(string printer, PrinterConditionChanged printerCondition)
+        public ConditionChangedEventArgs(string printer, PrinterConditionChanged printerCondition): base(printer)
         {
-            Printer = printer;
             PrinterCondition = printerCondition;
         }
-
-        public string Printer { get; }
         public PrinterConditionChanged PrinterCondition { get; }
     }
 
-    public sealed class SettingChangedEventArgs : EventArgs
+    public sealed class SettingChangedEventArgs : PrinterEventArgs
     {
-        public SettingChangedEventArgs(string printer, PrinterSettingChanged settingChangedChanged)
+        public SettingChangedEventArgs(string printer, PrinterSettingChanged settingChangedChanged): base(printer)
         {
-            Printer = printer;
             SettingChangedChanged = settingChangedChanged;
         }
-
-        public string Printer { get; }
         public PrinterSettingChanged SettingChangedChanged { get; }
     }
 }
