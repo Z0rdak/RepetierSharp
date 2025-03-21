b32bf2d3a2f98887cc954a8d8ff5c441f0b1f269
Marcel Bartholet <Marcel.Bartholet@mni.thm.de>
2025-03-21 22:35:31 +0100

    update md5 password hash

--- RepetierSharp/Util/CommandHelper.cs
+++ RepetierSharp/Util/CommandHelper.cs
@@ -2,30 +2,26 @@
 
 namespace RepetierSharp.Util
 {
-    internal class CommandHelper
+    internal static class CommandHelper
     {
         /// <summary>
         ///     See: https://stackoverflow.com/questions/11454004/calculate-a-md5-hash-from-a-string
         /// </summary>
         /// <param name="input"></param>
         /// <returns></returns>
-        internal static string MD5(string input)
+        private static string MD5(string input)
         {
             // Use input string to calculate MD5 hash
-            using ( var md5 = System.Security.Cryptography.MD5.Create() )
+            var inputBytes = Encoding.ASCII.GetBytes(input);
+            var hashBytes = System.Security.Cryptography.MD5.HashData(inputBytes);
+
+            // Convert the byte array to hexadecimal string
+            var sb = new StringBuilder();
+            for ( var i = 0; i < hashBytes.Length; i++ )
             {
-                var inputBytes = Encoding.ASCII.GetBytes(input);
-                var hashBytes = md5.ComputeHash(inputBytes);
-
-                // Convert the byte array to hexadecimal string
-                var sb = new StringBuilder();
-                for ( var i = 0; i < hashBytes.Length; i++ )
-                {
-                    sb.Append(hashBytes[i].ToString("x2"));
-                }
-
-                return sb.ToString();
+                sb.Append(hashBytes[i].ToString("x2"));
             }
+            return sb.ToString();
         }
 
         public static string HashPassword(string sessionKey, string login, string password)
