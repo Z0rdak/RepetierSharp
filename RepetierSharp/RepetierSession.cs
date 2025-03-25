a25aea400a5a018a2b18aa01b8a51773792ed6ca
Marcel Bartholet <Marcel.Bartholet@mni.thm.de>
2025-03-25 11:11:29 +0100

    move password hashing into credential auth - still need to test if this can be removed entirely

--- RepetierSharp/RepetierSession.cs
+++ RepetierSharp/RepetierSession.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace RepetierSharp
 {
@@ -34,6 +35,34 @@ namespace RepetierSharp
         public AuthenticationType AuthType { get => AuthenticationType.Credentials; }
         public string LoginName { get; set; } = loginName;
         public string Password { get; set; } = password;
+        
+        /// <summary>
+        ///     See: https://stackoverflow.com/questions/11454004/calculate-a-md5-hash-from-a-string
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="encoding"></param>
+        /// <returns></returns>
+        private static string MD5(string input, Encoding? encoding = null)
+        {
+            if (encoding == null)
+                encoding = Encoding.ASCII;
+            // Use input string to calculate MD5 hash
+            var inputBytes = encoding.GetBytes(input);
+            var hashBytes = System.Security.Cryptography.MD5.HashData(inputBytes);
+
+            // Convert the byte array to hexadecimal string
+            var sb = new StringBuilder();
+            for ( var i = 0; i < hashBytes.Length; i++ )
+            {
+                sb.Append(hashBytes[i].ToString("x2"));
+            }
+            return sb.ToString();
+        }
+
+        public static string HashPassword(string sessionKey, string login, string password)
+        {
+            return MD5(sessionKey + MD5(login + password, Encoding.ASCII), Encoding.ASCII);
+        }
     }
     
     public enum AuthenticationType
